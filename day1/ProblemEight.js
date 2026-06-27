8ac472356a65b07f68c6c83936c45b03413993a6
shanto <shantokumar22@gmail.com>
2026-06-27 13:03:11 +0600

    feat: add solution files and problem description for day 1 tasks

---
+++ day1/ProblemEight.js
@@ -0,0 +1,12 @@
+// প্রশ্ন ৮: কিভাবে একটি অ্যারের সবগুলো সংখ্যার যোগফল (Sum) বের করা যায়?
+// Question 8: How to calculate the sum of all numbers in an array?
+
+// const ArrayNumber = [5,1,2,2,3]
+
+// let Sum = 0
+
+// ArrayNumber.forEach(function(value){
+//         Sum += value
+// })
+
+// console.log(`All Number Total Sum is : ${Sum}`);
---
+++ day1/Problemseven.js
@@ -0,0 +1,13 @@
+// প্রশ্ন ৭: কিভাবে একটি সংখ্যার অ্যারে (Array) থেকে সবচেয়ে বড় সংখ্যা (Max number) বের করা যায়?
+// Question 7: How to find the maximum number from an array of numbers?
+
+
+// const Numbers=[5,15,36,4,5,84,61,100]
+
+// let Max = 0
+// Numbers.forEach(function(value){
+//     if(Max < value){
+//         Max = value
+//     }
+// })
+// console.log(`The max value is ${Max}`);
